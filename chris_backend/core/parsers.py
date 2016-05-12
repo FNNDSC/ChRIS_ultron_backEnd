0edc37e6f446925b52b0cf486c5e1eaa7796c4ca
Jorge <jbernal0019@yahoo.es>
2016-05-12 16:20:45 -0400

    CollectionJsonParser now handles bad requests

--- chris_backend/core/parsers.py
+++ chris_backend/core/parsers.py
@@ -1,10 +1,22 @@
-
 from rest_framework.parsers import JSONParser
+from rest_framework.exceptions import ParseError
 
 class CollectionJsonParser(JSONParser):
     media_type = 'application/vnd.collection+json'
+
+    def validate_data(self, stream_data):
+        json_data = {}
+        try:
+            for x in stream_data['template']['data']:
+                json_data[x['name']] = x['value']
+        except KeyError as e:
+            detail = "%s field required in request: {template:{data:[{name: ,value: },...]}}" % e  
+            raise ParseError(detail=detail)
+        return json_data 
    
     def parse(self, stream, media_type=None, parser_context=None):
+        stream_data = super(CollectionJsonParser, self).parse(stream, media_type,
+                                                          parser_context)         
+        return self.validate_data(stream_data)
 
-        return super(CollectionJsonParser, self).parse(stream, media_type,
-                                                          parser_context)
+
