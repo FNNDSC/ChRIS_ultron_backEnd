e281d8d4c4dbb9d559118ae919fec5f1c910a31f
Jorge <jbernal0019@yahoo.es>
2016-06-23 14:00:53 -0400

    Added has_permission method which is necessary for permissions in ListViews

--- chris_backend/plugins/permissions.py
+++ chris_backend/plugins/permissions.py
@@ -7,14 +7,17 @@ class IsChrisOrReadOnly(permissions.BasePermission):
     Read only is allowed to other users.
     """
 
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
 
         # Write permissions are only allowed to the superuser 'chris'.
-        return request.user.username == 'chris'   
+        return request.user.username == 'chris' 
+
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)   
 
 
 
