dc8fe0919e45b390dd7d6ded6dcdec0c9be917ce
Jorge <jbernal0019@yahoo.es>
2016-06-23 16:48:21 -0400

    Now CollectionJsonParser checks if the uploaded template is a dict. Added correspondig tests

--- chris_backend/collectionjson/parsers.py
+++ chris_backend/collectionjson/parsers.py
@@ -5,13 +5,21 @@ class CollectionJsonParser(JSONParser):
     media_type = 'application/vnd.collection+json'
 
     def validate_data(self, stream_data):
+        template_valid_str = "Valid format: {template:{data:[{name: ,value: },...]}}"
+        
+        if not isinstance(stream_data, dict):
+            detail = "Template is not a dictionary. "
+            detail += template_valid_str
+            raise ParseError(detail=detail)
+        
         json_data = {}
         try:
             for x in stream_data['template']['data']:
                 json_data[x['name']] = x['value']
         except KeyError as e:
-            detail = "%s field required in request: {template:{data:[{name: ,value: },...]}}" % e  
-            raise ParseError(detail=detail)
+            detail = "%s field required. " % e 
+            detail += template_valid_str  
+            raise ParseError(detail=detail)        
         return json_data 
    
     def parse(self, stream, media_type=None, parser_context=None):
--- chris_backend/collectionjson/tests/test_parsers.py
+++ chris_backend/collectionjson/tests/test_parsers.py
@@ -5,12 +5,9 @@ from django.conf.urls import url, include
 from django.test.utils import override_settings
 from django.test import TestCase
 
-from collection_json import Collection
 from rest_framework import status
 from rest_framework.routers import DefaultRouter
 
-from collectionjson.renderers import CollectionJsonRenderer
-from .models import Moron
 from . import views
 
 
@@ -20,9 +17,6 @@ class SimplePOSTTest(TestCase):
 
     def setUp(self):
         self.content_type='application/vnd.collection+json'
-        self.post = json.dumps({"template": {"data": [{"name": "name", "value": "Bob"}]}})
-        self.response = self.client.post(self.endpoint, data=self.post,
-                                    content_type=self.content_type)
 
 
 class TestCollectionJsonParser(SimplePOSTTest):
@@ -32,8 +26,23 @@ class TestCollectionJsonParser(SimplePOSTTest):
         super(TestCollectionJsonParser, self).setUp()
 
     def test_create_success(self):
-        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(self.response.data["name"], "Bob")
+        post = json.dumps({"template": {"data": [{"name": "name", "value": "Bob"}]}})
+        response = self.client.post(self.endpoint, data=post,
+                                    content_type=self.content_type)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["name"], "Bob")
+
+    def test_create_failure_invalid_template_not_dict(self):
+        post = json.dumps([{"template": {"data": [{"name": "name", "value": "Bob"}]}}])
+        response = self.client.post(self.endpoint, data=post,
+                                    content_type=self.content_type)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_failure_invalid_template_missing_field(self):
+        post = json.dumps({"templlte": {"data": [{"name": "name", "value": "Bob"}]}})
+        response = self.client.post(self.endpoint, data=post,
+                                    content_type=self.content_type)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
 router = DefaultRouter()
