2535234018da50e874ad175ebb138aca1152947a
Jorge <jbernal0019@yahoo.es>
2016-07-28 16:05:29 -0400

    Removed tests for non-readonly requests to PluginList, PluginDetail, PluginParameterList and PluginParameterDetail views

--- chris_backend/plugins/tests/test_views.py
+++ chris_backend/plugins/tests/test_views.py
@@ -40,37 +40,16 @@ class PluginListViewTests(ViewTests):
 
     def setUp(self):
         super(PluginListViewTests, self).setUp()     
-        self.create_read_url = reverse("plugin-list")
-        self.post = json.dumps({"template": {"data": [{"name": "name", "value": "mri_deface"},
-                                          {"name": "type", "value": "ds"}]}})
-
-    def test_plugin_create_success(self):
-        self.client.login(username=self.chris_username, password=self.chris_password)
-        response = self.client.post(self.create_read_url, data=self.post,
-                                    content_type=self.content_type)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data["name"], "mri_deface")
-        self.assertEqual(response.data["type"], "ds")
-
-    def test_plugin_create_failure_user_is_not_chris(self):
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.post(self.create_read_url, data=self.post,
-                                    content_type=self.content_type)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_plugin_create_failure_unauthenticated(self):
-        response = self.client.post(self.create_read_url, data=self.post,
-                                    content_type=self.content_type)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.list_url = reverse("plugin-list")
 
     def test_plugin_list_success(self):
         self.client.login(username=self.username, password=self.password)
-        response = self.client.get(self.create_read_url)
+        response = self.client.get(self.list_url)
         self.assertContains(response, "pacspull")
         self.assertContains(response, "mri_convert")
 
     def test_plugin_list_failure_unauthenticated(self):
-        response = self.client.get(self.create_read_url)
+        response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
@@ -83,49 +62,15 @@ class PluginDetailViewTests(ViewTests):
         super(PluginDetailViewTests, self).setUp()     
         plugin = Plugin.objects.get(name="pacspull")
         
-        self.read_update_delete_url = reverse("plugin-detail", kwargs={"pk": plugin.id})
-        self.put = json.dumps({
-            "template": {"data": [{"name": "name", "value": "pacspull_updated"}]}})
+        self.read_url = reverse("plugin-detail", kwargs={"pk": plugin.id})
           
     def test_plugin_detail_success(self):
         self.client.login(username=self.username, password=self.password)
-        response = self.client.get(self.read_update_delete_url)
+        response = self.client.get(self.read_url)
         self.assertContains(response, "pacspull")
 
     def test_plugin_detail_failure_unauthenticated(self):
-        response = self.client.get(self.read_update_delete_url)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_plugin_update_success(self):
-        self.client.login(username=self.chris_username, password=self.chris_password)
-        response = self.client.put(self.read_update_delete_url, data=self.put,
-                                   content_type=self.content_type)
-        self.assertContains(response, "pacspull_updated")
-
-    def test_plugin_update_failure_user_is_not_chris(self):
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.put(self.read_update_delete_url, data=self.put,
-                                   content_type=self.content_type)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_plugin_update_failure_unauthenticated(self):
-        response = self.client.put(self.read_update_delete_url, data=self.put,
-                                   content_type=self.content_type)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_plugin_delete_success(self):
-        self.client.login(username=self.chris_username, password=self.chris_password)
-        response = self.client.delete(self.read_update_delete_url)
-        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEquals(Plugin.objects.count(), 1)
-
-    def test_plugin_delete_failure_user_is_not_chris(self):
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.delete(self.read_update_delete_url)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_plugin_delete_failure_unauthenticated(self):
-        response = self.client.delete(self.read_update_delete_url)
+        response = self.client.get(self.read_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
@@ -138,9 +83,7 @@ class PluginParameterListViewTests(ViewTests):
         super(PluginParameterListViewTests, self).setUp()
         plugin = Plugin.objects.get(name="pacspull")
         #self.corresponding_plugin_url = reverse("plugin-detail", kwargs={"pk": plugin.id})
-        self.create_read_url = reverse("pluginparameter-list", kwargs={"pk": plugin.id})
-        self.post = json.dumps({"template": {"data": [{"name": "name", "value": "mrn"},
-                                          {"name": "type", "value": "string"}]}})
+        self.list_url = reverse("pluginparameter-list", kwargs={"pk": plugin.id})
 
         # create two plugin parameters
         PluginParameter.objects.get_or_create(plugin=plugin,
@@ -148,33 +91,14 @@ class PluginParameterListViewTests(ViewTests):
         PluginParameter.objects.get_or_create(plugin=plugin,
                                               name='prefix', type='string')
 
-    def test_plugin_parameter_create_success(self):
-        self.client.login(username=self.chris_username, password=self.chris_password)
-        response = self.client.post(self.create_read_url, data=self.post,
-                                    content_type=self.content_type)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data["name"], "mrn")
-        self.assertEqual(response.data["type"], "string")
-
-    def test_plugin_parameter_create_failure_user_is_not_chris(self):
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.post(self.create_read_url, data=self.post,
-                                    content_type=self.content_type)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_plugin_parameter_create_failure_unauthenticated(self):
-        response = self.client.post(self.create_read_url, data=self.post,
-                                    content_type=self.content_type)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
     def test_plugin_parameter_list_success(self):
         self.client.login(username=self.username, password=self.password)
-        response = self.client.get(self.create_read_url)
+        response = self.client.get(self.list_url)
         self.assertContains(response, "img_type")
         self.assertContains(response, "prefix")
 
     def test_plugin_parameter_list_failure_unauthenticated(self):
-        response = self.client.get(self.create_read_url)
+        response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
@@ -189,50 +113,16 @@ class PluginParameterDetailViewTests(ViewTests):
         # create a plugin parameter
         (param, tf) = PluginParameter.objects.get_or_create(plugin=plugin,
                                                                 name='mrn', type='string')  
-        self.read_update_delete_url = reverse("pluginparameter-detail",
+        self.read_url = reverse("pluginparameter-detail",
                                               kwargs={"pk": param.id})
-        self.put = json.dumps({
-            "template": {"data": [{"name": "name", "value": "mrn_updated"}]}})
           
     def test_plugin_parameter_detail_success(self):
         self.client.login(username=self.username, password=self.password)
-        response = self.client.get(self.read_update_delete_url)
+        response = self.client.get(self.read_url)
         self.assertContains(response, "mrn")
 
     def test_plugin_parameter_detail_failure_unauthenticated(self):
-        response = self.client.get(self.read_update_delete_url)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_plugin_parameter_update_success(self):
-        self.client.login(username=self.chris_username, password=self.chris_password)
-        response = self.client.put(self.read_update_delete_url, data=self.put,
-                                   content_type=self.content_type)
-        self.assertContains(response, "mrn_updated")
-
-    def test_plugin_parameter_update_failure_user_is_not_chris(self):
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.put(self.read_update_delete_url, data=self.put,
-                                   content_type=self.content_type)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_plugin_parameter_update_failure_unauthenticated(self):
-        response = self.client.put(self.read_update_delete_url, data=self.put,
-                                   content_type=self.content_type)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_plugin_parameter_delete_success(self):
-        self.client.login(username=self.chris_username, password=self.chris_password)
-        response = self.client.delete(self.read_update_delete_url)
-        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEquals(PluginParameter.objects.count(), 0)
-
-    def test_plugin_parameter_delete_failure_user_is_not_chris(self):
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.delete(self.read_update_delete_url)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_plugin_parameter_delete_failure_unauthenticated(self):
-        response = self.client.delete(self.read_update_delete_url)
+        response = self.client.get(self.read_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
