2a8e5f3fac93504993fda88d01e4e4667773a179
nicolasrannou <npm.rannou@gmail.com>
2016-09-13 15:00:37 +0200

    Update Vagrant provision script for https

--- utils/vagrant/bootstrap_root.sh
+++ utils/vagrant/bootstrap_root.sh
@@ -11,6 +11,9 @@ apt-get install debconf-utils -y
 debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
 
+echo "Installing Apache2 - dev"
+apt-get install apache2-dev -y
+
 echo "Installing MySQL"
 apt-get install mysql-server libmysqlclient-dev -y
 
