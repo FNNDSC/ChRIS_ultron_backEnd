73101ca6bb3c49401e9cbc2198d8201da746ab7f
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2016-09-27 16:19:30 -0400

    Change behaviour to "touch" files in 'ls' exec.

--- chris_backend/plugins/services/simplefsapp/simplefsapp.py
+++ chris_backend/plugins/services/simplefsapp/simplefsapp.py
@@ -8,14 +8,16 @@
 #                        dev@babyMRI.org
 #
 
-
-
 import os, sys
 
 # import the Chris app superclass
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 from base import ChrisApp
 
+
+def touch(path):
+    with open(path, 'a'):
+        os.utime(path, None)
 
 class SimpleFSApp(ChrisApp):
     """
@@ -38,8 +40,18 @@ class SimpleFSApp(ChrisApp):
                           optional=True, help='look up directory')
 
     def run(self, options):
-        print(os.system('ls ' + options.dir + '>' + os.path.join(options.outputdir,
-                                                                 'out.txt')))
+        str_outFile = os.path.join(options.outputdir, 'out.txt')
+        print(os.system('ls ' + options.dir + '>' + str_outFile))
+
+        # Create a 'dummy' listing of empty files mirroring the target dir listing
+        with open(str_outFile) as f:
+            l_ls    = f.readlines()
+        print(l_ls)
+        l_ls = map(str.strip, l_ls)
+        for str_file in l_ls:
+            str_fullPath    = os.path.join(options.outputdir, str_file)
+            print('touching file... %s' % str_fullPath)
+            touch(str_fullPath)
 
 
 # ENTRYPOINT
