ffbc1e9a01d23deddca3d2480c0c229a5081f2ef
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2016-11-01 11:06:33 -0400

    Remove old pman settings.

--- chris_backend/plugins/views.py
+++ chris_backend/plugins/views.py
@@ -13,13 +13,6 @@ from .serializers import PluginInstanceSerializer
 from .permissions import IsChrisOrReadOnly
 from .services.manager import PluginManager
 
-
-import  socket
-# This will need to be better addressed!
-class pman_settings():
-    HOST    =  [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
-    PORT    = '5010'
-
 class PluginList(generics.ListAPIView):
     """
     A view for the collection of plugins.
