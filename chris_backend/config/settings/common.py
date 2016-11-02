02b95c1a79e6945ddbcb16fd61adecd76323a3a8
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2016-11-02 14:15:54 -0400

    PMAN settings and minor indent formatting.

--- chris_backend/config/settings/common.py
+++ chris_backend/config/settings/common.py
@@ -24,7 +24,7 @@ ALLOWED_HOSTS = []
 
 # pman settings
 PMAN    = {
-    'host': 'localhost',
+    'host': '127.0.0.1',
     'port': 5010
 }
 
@@ -49,20 +49,20 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DEFAULT_RENDERER_CLASSES': (
-    'collectionjson.renderers.CollectionJsonRenderer',
-    'rest_framework.renderers.JSONRenderer',
-    'rest_framework.renderers.BrowsableAPIRenderer',
+        'collectionjson.renderers.CollectionJsonRenderer',
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_PARSER_CLASSES': (
-    'collectionjson.parsers.CollectionJsonParser',
-    'rest_framework.parsers.JSONParser',
-    'rest_framework.parsers.FormParser',
-    'rest_framework.parsers.MultiPartParser',
+        'collectionjson.parsers.CollectionJsonParser',
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-    'rest_framework.authentication.TokenAuthentication',
-    'rest_framework.authentication.BasicAuthentication',
-    'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     )
 }
 
