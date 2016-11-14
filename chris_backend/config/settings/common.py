a6da482009765ee42c3b16c7b6e28469a67961ae
Jorge <jbernal0019@yahoo.es>
2016-11-14 15:46:46 +0000

    Set pman host variable in common.py

--- chris_backend/config/settings/common.py
+++ chris_backend/config/settings/common.py
@@ -23,10 +23,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ALLOWED_HOSTS = []
 
 # pman settings
-PMAN    = {
-    'host': '127.0.0.1',
+# Set the pman IP to specific host IP.
+try:    
+    PMAN    = {
+    'host': [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0],
     'port': 5010
-}
+    }
+except Exception:
+    PMAN    = {
+        'host': '127.0.0.1',
+        'port': 5010
+    }
 
 # Application definition
 
