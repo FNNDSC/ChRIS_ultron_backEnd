fdff3c12b670806df21d22a64e254b658bfa6588
Jorge <jbernal0019@yahoo.es>
2016-11-14 21:44:16 +0000

    Set DEFAULT_FILTER_BACKENDS

--- chris_backend/config/settings/common.py
+++ chris_backend/config/settings/common.py
@@ -69,6 +69,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
     )
 }
 
