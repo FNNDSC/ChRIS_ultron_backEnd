8db3afd89dcf5b34d483069d953f88091186af56
Jorge <jbernal0019@yahoo.es>
2017-02-01 14:07:58 -0500

    Now waits until MySql server is ready

--- docker-make-chris_dev.sh
+++ docker-make-chris_dev.sh
@@ -9,8 +9,11 @@ echo "1-Creating a volume container for the chrid db data"
 docker run -d -v /var/lib/mysql --name chris_dev_db_data alpine echo DB Data Container
 echo " "
 
-echo "2-Starting mysql in a container mounting the previous volume container and docker-init-chris_dev_db.sh file ..."
+echo "2-Starting mysql in a container mounting the previous volume container and docker-init-chris_dev_db.sh file"
 docker run --name chris_dev_db --volumes-from chris_dev_db_data -v "$PWD/docker-init-chris_dev_db.sh":/docker-init-chris_dev_db.sh -e MYSQL_ROOT_PASSWORD=rootp -d mysql:5
+echo " "
+echo "Waiting until mysql server is ready to accept connections ..."
+docker exec chris_dev_db sh -c 'while ! mysqladmin -uroot -prootp status 2> /dev/null; do sleep 5; done;'
 echo " "
 
 echo "3-Starting chris development environment container with access to the previous mysql container ..."
@@ -19,8 +22,6 @@ export chris_dev_IP="$(docker inspect --format '{{.NetworkSettings.IPAddress}}' 
 echo " "
 
 echo "4-Initializing chris_dev DB" 
-echo "Waiting 30 seconds for chris development server to stabilize..."
-sleep 30
 docker exec chris_dev_db ./docker-init-chris_dev_db.sh $chris_dev_IP
 echo " "
 
@@ -28,15 +29,15 @@ echo "5-Making migrations"
 docker exec chris_dev python3 manage.py migrate
 echo " "
 
-echo "6-Registering plugins"
+echo "6-Registering plugins ..."
 docker exec chris_dev sh -c 'for i in $(ls -d plugins/services/*/); do if [ -f ${i}$(basename ${i}).py ]; then python3 plugins/services/manager.py --add $(basename ${i}) 2> /dev/null; fi; done'
 echo " "
 
-echo "6-Restarting Djando development server ..."
+echo "7-Restarting Djando development server ..."
 docker restart chris_dev
 echo " "
 
-echo "7-Now create three chris API users"
+echo "8-Now create three chris API users"
 echo 'Please name one of the users as "chris"' 
 docker exec -it chris_dev python3 manage.py createsuperuser
 docker exec -it chris_dev python3 manage.py createsuperuser
