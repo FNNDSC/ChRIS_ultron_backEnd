275d7d139024663252a57e0febc66d60af40d5db
Jorge <jbernal0019@yahoo.es>
2017-03-23 16:24:46 -0400

    The list of plugins to be automatically registered at startup is now in an array in the script

--- docker-make-chris_dev.sh
+++ docker-make-chris_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "1-Changing permissions to 755 for $PWD ..."
-chmod 755 -R $(pwd)
+chmod -R 755 $(pwd)
 echo " "
 
 echo "2-Starting chris containerized development environment from ./docker-compose.yml ..."
@@ -11,7 +11,7 @@ echo " "
 
 echo "3-Waiting until mysql server is ready to accept connections ..."
 docker-compose exec chris_dev_db sh -c 'while ! mysqladmin -uroot -prootp status 2> /dev/null; do sleep 5; done;'
-# Give allpermissions to chris user in the DB. This is required for the Django tests:
+# Give all permissions to chris user in the DB. This is required for the Django tests:
 docker-compose exec chris_dev_db mysql -uroot -prootp -e 'GRANT ALL PRIVILEGES ON *.* TO "chris"@"%"'
 echo " "
 
@@ -20,7 +20,13 @@ docker-compose exec chris_dev python manage.py migrate
 echo " "
 
 echo "5-Registering plugins ..."
-docker-compose exec chris_dev sh -c 'for i in $(ls -d plugins/services/*/); do if [ -f ${i}$(basename ${i}).py ]; then python3 plugins/services/manager.py --add $(basename ${i}) 2> /dev/null; fi; done'
+# Declare an array variable for the list of plugin dock images
+# Add a new plugin image name to the list if you want it to be automatically registered
+docker-compose exec chris_dev /bin/bash -c \
+  'declare -a plugins=("fnndsc/pl-simplefsapp"
+                       "fnndsc/pl-simpledsapp"
+                      )
+   for plugin in "${plugins[@]}"; do python3 plugins/services/manager.py --add ${plugin} 2> /dev/null; done'
 echo " "
 
 echo "6-Running Django tests ..."
@@ -31,9 +37,9 @@ echo "7-Restarting Djando development server ..."
 docker-compose restart chris_dev
 echo " "
 
-echo "8-Now create three chris API users"
+echo "8-Now create two chris API users"
 echo 'Please name one of the users as "chris"'
 echo " "
 docker-compose exec chris_dev python manage.py createsuperuser
 docker-compose exec chris_dev python manage.py createsuperuser
-docker-compose exec chris_dev python manage.py createsuperuser
+
