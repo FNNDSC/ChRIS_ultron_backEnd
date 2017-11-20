5b92164ed051ece038fb0c50aa4d53fde01e4681
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-11-20 16:09:25 -0500

    Remove decoration noise and add RELEASE handling.

--- docker-make-chris_dev.sh
+++ docker-make-chris_dev.sh
@@ -6,7 +6,7 @@
 #
 # SYNPOSIS
 #
-#   docker-make-chris_dev.sh [local|fnndsc]
+#   docker-make-chris_dev.sh [local|fnndsc[:dev]]
 #
 # DESC
 # 
@@ -18,9 +18,12 @@
 #
 # ARGS
 #
-#   [local|fnndsc] (optional, default = 'fnndsc')
+#   [local|fnndsc[:dev]] (optional, default = 'fnndsc')
 #
-#       If specified, denotes the container "family" to use. 
+#       If specified, denotes the container "family" to use.
+#
+#       If a colon suffix exists, then this is interpreted to further
+#       specify the release.
 #
 #       The 'fnndsc' family are the containers as hosted on docker hub. 
 #       Using 'fnndsc' will always attempt to pull the latest container first.
@@ -30,143 +33,19 @@
 #       the 'pfcon/pman/pfioh/pfurl' services are being locally 
 #       developed/debugged.
 #
+#       
 
-   ESC(){ echo -en "\033";}                            # escape character
- CLEAR(){ echo -en "\033c";}                           # the same as 'tput clear'
- CIVIS(){ echo -en "\033[?25l";}                       # the same as 'tput civis'
- CNORM(){ echo -en "\033[?12l\033[?25h";}              # the same as 'tput cnorm'
-  TPUT(){ echo -en "\033[${1};${2}H";}                 # the same as 'tput cup'
-COLPUT(){ echo -en "\033[${1}G";}                      # put text in the same line as the specified column
-  MARK(){ echo -en "\033[7m";}                         # the same as 'tput smso'
-UNMARK(){ echo -en "\033[27m";}                        # the same as 'tput rmso'
-  DRAW(){ echo -en "\033%";echo -en "\033(0";}        # switch to 'garbage' mode
- WRITE(){ echo -en "\033(B";}                          # return to normal mode from 'garbage' on the screen
-  BLUE(){ echo -en "\033c\033[0;1m\033[37;44m\033[J";} # reset screen, set background to blue and font to white
 
-# Foreground
-RED='\033[0;31m'
-NC='\033[m' # No Color
-Black='\033[0;30m'     
-DarkGray='\033[1;30m'
-Red='\033[0;31m'     
-LightRed='\033[1;31m'
-Green='\033[0;32m'     
-LightGreen='\033[1;32m'
-Brown='\033[0;33m'     
-Yellow='\033[1;33m'
-Blue='\033[0;34m'     
-LightBlue='\033[1;34m'
-Purple='\033[0;35m'     
-LightPurple='\033[1;35m'
-Cyan='\033[0;36m'     
-LightCyan='\033[1;36m'
-LightGray='\033[0;37m'     
-White='\033[1;37m'
-
-# Background
-NC='\033[m' # No Color
-BlackBG='\033[0;40m'     
-DarkGrayBG='\033[1;40m'
-RedBG='\033[0;41m'     
-LightRedBG='\033[1;41m'
-GreenBG='\033[0;42m'     
-LightGreenBG='\033[1;42m'
-BrownBG='\033[0;43m'     
-YellowBG='\033[1;43m'
-BlueBG='\033[0;44m'     
-LightBlueBG='\033[1;44m'
-PurpleBG='\033[0;45m'     
-LightPurpleBG='\033[1;45m'
-CyanBG='\033[0;46m'     
-LightCyanBG='\033[1;46m'
-LightGrayBG='\033[0;47m'     
-WhiteBG='\033[1;47m'
-
+source ./decorate.sh 
 
 declare -i STEP=0
 declare -i b_restart=0
 RESTART=""
 HERE=$(pwd)
 echo "Starting script in dir $HERE"
+
 CREPO=fnndsc
-
-declare -a A_CONTAINER=(
-    "chris_dev_backend"
-    "pfcon:dev"
-    "pfurl:dev"
-    "pfioh:dev"
-    "pman:dev"
-    "swarm"
-    "pfdcm:dev"
-    "pl-pacsquery"
-    "pl-pacsretrieve"
-)
-
-function title {
-    declare -i b_date=0
-    local OPTIND
-    while getopts "d:" opt; do
-        case $opt in 
-            d) b_date=$OPTARG ;;
-        esac
-    done
-    shift $(($OPTIND - 1))
-
-    STEP=$(expr $STEP + 1 )
-    MSG="$1"
-    MSG2="$2"
-    TITLE=$(echo " $STEP.0: $MSG ")
-    LEN=$(echo "$TITLE" | awk -F\| {'printf("%s", length($1));'})
-    if ! (( LEN % 2 )) ; then
-        TITLE="$TITLE "
-    fi
-    MSG=$(echo -e "$TITLE" | awk -F\| {'printf("%*s%*s\n", 39+length($1)/2, $1, 40-length($1)/2, "");'})
-    if (( ${#MSG2} )) ; then
-        TITLE2=$(echo " $MSG2 ")
-        LEN2=$(echo "$TITLE2" | awk -F\| {'printf("%s", length($1));'})
-        MSG2=$(echo -e "$TITLE2" | awk -F\| {'printf("%*s%*s\n", 39+length($1)/2, $1, 40-length($1)/2, "");'})
-    fi
-    printf "\n"
-    DATE=$(date)
-    DRAW
-    if (( b_date )) ; then
-        printf "${LightBlue}lqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqk\nx"
-        WRITE
-        printf "%-30s" "$DATE"
-        DRAW 
-        printf "x\n"
-        printf "${Yellow}tqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqvqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqk\n"
-    else
-        printf "${Yellow}lqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqk\n"
-    fi
-    printf "x"
-    WRITE
-    printf "${LightPurple}$MSG${Yellow}"
-    if (( ${#MSG2} )) ; then
-        DRAW
-        printf "x\nx"
-        WRITE
-        printf "${LightPurple}$MSG2${Yellow}"
-    fi
-    DRAW
-    printf "x\n"
-    printf "${Yellow}tqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqu\n"
-    WRITE
-    printf "${NC}"
-}
-
-function windowBottom {
-    # printf "a b c d f e f g h i j k l m n o p q r s t u v w x y z]\n"
-    DRAW
-    # printf "a b c d f e f g h i j k l m n o p q r s t u v w x y z]\n"
-    printf "${Yellow}"
-    printf "mwqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqv${Brown}qk\n"
-    # printf "mqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj\n"
-    printf "${Brown}"
-    printf " mqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj\n"
-    WRITE
-    printf "${NC}"
-}
+REL=:dev
 
 while getopts "r:" opt; do
     case $opt in 
@@ -176,11 +55,24 @@ while getopts "r:" opt; do
 done
 shift $(($OPTIND - 1))
 if (( $# == 1 )) ; then
-    CREPO=$1
+    REPO=$1
 fi
-export CREPO=$CREPO
+export CREPO=$(echo $REPO | awk -F \: '{print $1}')
+export RELEASE=$(echo $REPO | awk -F \: '{print ":"$2}')
 
-title -d 1 "Using <$CREPO> containers..."
+declare -a A_CONTAINER=(
+    "chris_dev_backend"
+    "pfcon${RELEASE}"
+    "pfurl${RELEASE}"
+    "pfioh${RELEASE}"
+    "pman${RELEASE}"
+    "swarm"
+    "pfdcm${RELEASE}"
+    "pl-pacsquery"
+    "pl-pacsretrieve"
+)
+
+title -d 1 "Using <$REPO> family containers..."
 if [[ $CREPO == "fnndsc" ]] ; then
     echo "Pulling latest version of all containers..."
     for CONTAINER in ${A_CONTAINER[@]} ; do
@@ -211,8 +103,8 @@ else
         fi
     done
     # And for the version of pfurl *inside* pfcon!
-    CMD="docker run --entrypoint /usr/local/bin/pfurl ${CREPO}/pfcon:dev --version"
-    printf "${White}%40s\t\t" "pfurl inside ${CREPO}/pfcon:dev"
+    CMD="docker run --entrypoint /usr/local/bin/pfurl ${CREPO}/pfcon${RELEASE} --version"
+    printf "${White}%40s\t\t" "pfurl inside ${CREPO}/pfcon${RELEASE}"
     Ver=$(echo $CMD | sh | grep Version)
     echo -e "$Green$Ver"
     CMD="docker run --entrypoint /usr/local/bin/pfurl ${CREPO}/chris_dev_backend --version"
@@ -261,21 +153,10 @@ else
     chmod 777 FS/local
     mkdir -p FS/remote
     chmod 777 FS/remote
-    # mkdir -p FS/users
-    # chmod 777 FS/users
     chmod 777 FS
     cd FS/remote
     echo -e "${STEP}.3 For pman override to swarm containers, exporting\n\tSTOREBASE=$(pwd)... "
     export STOREBASE=$(pwd)
-    # chmod 777 FS/users
-    # echo "1.3: Create tree structure to emulate volume mapping"
-    # echo "1.3: This allows for easy switching/running between"
-    # echo "1.3: containerized and non-containerized environments."
-    # sudo rm -fr /hostFS 2>/dev/null
-    # sudo mkdir -p /hostFS
-    # cd /hostFS
-    # sudo ln -s ${HERE}/FS/local   pfconFS
-    # sudo ln -s ${HERE}/FS/remote  storeBase
     cd $HERE
     windowBottom
 
