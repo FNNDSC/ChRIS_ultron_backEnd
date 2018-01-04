11b3d4db0ff2b39c46b19a6cc1d2ca213434a98d
Jorge <jbernal0019@yahoo.es>
2018-01-04 15:25:27 -0500

    Added test_uploaded_file_path and test_str unit tests

--- chris_backend/uploadedfiles/tests/test_models.py
+++ chris_backend/uploadedfiles/tests/test_models.py
@@ -1,3 +1,26 @@
 
+from unittest import mock
+
 from django.test import TestCase
 from django.contrib.auth.models import User
+from uploadedfiles.models import UploadedFile, uploaded_file_path
+
+
+class UploadedFileModelTests(TestCase):
+
+    def test_uploaded_file_path(self):
+        uploadedfile_instance = mock.Mock()
+        uploadedfile_instance.owner = mock.Mock(spec=User)
+        uploadedfile_instance.owner.username = 'foo'
+        uploadedfile_instance.upload_path = '/myuploads'
+        filename = 'file1.txt'
+        file_path = uploaded_file_path(uploadedfile_instance, filename)
+        expected_file_path = '{0}/{1}/{2}'.format(uploadedfile_instance.owner.username,
+                                                  'uploads',
+                                                  uploadedfile_instance.upload_path)
+        self.assertEquals(file_path, expected_file_path)
+
+    def test_str(self):
+        uploadedfile_mock = mock.MagicMock(spec=UploadedFile)
+        uploadedfile_mock.upload_path = '/myuploads'
+        self.assertEqual(UploadedFile.__str__(uploadedfile_mock), '/myuploads')
