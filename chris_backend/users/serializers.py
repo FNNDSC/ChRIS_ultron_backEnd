cce363cbd51171041f43e8aa5b3a037de81002ce
Jorge <jbernal0019@yahoo.es>
2018-01-11 16:49:27 -0500

    Added users' serializer and views

--- chris_backend/users/serializers.py
+++ chris_backend/users/serializers.py
@@ -2,12 +2,23 @@
 from django.contrib.auth.models import User
 
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     feed = serializers.HyperlinkedRelatedField(many=True, view_name='feed-detail',
                                                read_only=True)
 
+    username = serializers.CharField(max_length=32,
+                                     validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(min_length=6, max_length=100, write_only=True)
+
+    def create(self, validated_data):
+        user = User(username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
     class Meta:
         model = User
-        fields = ('url', 'username', 'feed')
+        fields = ('url', 'username', 'feed', 'password')
--- chris_backend/users/views.py
+++ chris_backend/users/views.py
@@ -5,12 +5,30 @@ from rest_framework import generics
 
 from .serializers import UserSerializer
 
-# Create your views here.
-class UserList(generics.ListAPIView):
+
+class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def perform_create(self, serializer):
+        """
+        Overriden to associate an owner, a plugin and a previous plugin instance with
+        the newly created plugin instance before first saving to the DB. All the plugin
+        instace's parameters in the resquest are also properly saved to the DB. Finally
+        the plugin's app is run with the provided plugin instance's parameters.
 
-class UserDetail(generics.RetrieveAPIView):
+        plugin = self.get_object()
+        request_data = serializer.context['request'].data
+        # get previous plugin instance
+        previous_id = ""
+        if 'previous_id' in request_data:
+            previous_id = request_data['previous_id']
+        previous = serializer.validate_previous(previous_id, plugin)
+        # create plugin instance with corresponding owner, plugin and previous instances
+        plugin_inst = serializer.save(owner=self.request.user, plugin=plugin,
+                                      previous=previous)
+        """
+
+class UserDetail(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
