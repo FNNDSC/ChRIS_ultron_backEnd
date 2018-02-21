bc997090eabaf8472dfb95cd9e6980f87fc7a0ff
Jorge <jbernal0019@yahoo.es>
2018-02-21 10:13:44 -0500

    Adjust the views and view tests to include user email

--- chris_backend/users/tests/test_views.py
+++ chris_backend/users/tests/test_views.py
@@ -17,6 +17,7 @@ class UserViewTests(TestCase):
         self.content_type = 'application/vnd.collection+json'
         self.username = 'cube'
         self.password = 'cubepass'
+        self.email = 'dev@babymri.org'
 
 
 class UserCreateViewTests(UserViewTests):
@@ -29,16 +30,20 @@ class UserCreateViewTests(UserViewTests):
         self.create_url = reverse("user-create")
         self.post = json.dumps(
             {"template": {"data": [{"name": "username", "value": self.username},
-                                   {"name": "password", "value": self.password}]}})
+                                   {"name": "password", "value": self.password},
+                                   {"name": "email", "value": self.email}]}})
 
     def test_user_create_success(self):
         response = self.client.post(self.create_url, data=self.post,
                                     content_type=self.content_type)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data["username"], self.username)
+        self.assertEqual(response.data["email"], self.email)
 
     def test_user_create_failure_already_exists(self):
-        User.objects.create_user(username=self.username, password=self.password)
+        User.objects.create_user(username=self.username,
+                                 email=self.email,
+                                 password=self.password)
         response = self.client.post(self.create_url, data=self.post,
                                     content_type=self.content_type)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -46,30 +51,11 @@ class UserCreateViewTests(UserViewTests):
     def test_user_create_failure_bad_password(self):
         post = json.dumps(
             {"template": {"data": [{"name": "username", "value": "new_user"},
+                                   {"name": "email", "value": self.email},
                                    {"name": "password", "value": "small"}]}})
         response = self.client.post(self.create_url, data=post,
                                     content_type=self.content_type)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-
-class UserListViewTests(UserViewTests):
-    """
-    Test the user-list view
-    """
-
-    def setUp(self):
-        super(UserListViewTests, self).setUp()
-        self.read_url = reverse("user-list")
-
-    def test_user_list_success(self):
-        User.objects.create_user(username=self.username, password=self.password)
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.get(self.read_url)
-        self.assertContains(response, self.username)
-
-    def test_user_list_failure_unauthenticated(self):
-        response = self.client.get(self.read_url)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class UserDetailViewTests(UserViewTests):
@@ -80,15 +66,18 @@ class UserDetailViewTests(UserViewTests):
     def setUp(self):
         super(UserDetailViewTests, self).setUp()
         user = User.objects.create_user(username=self.username,
+                                        email=self.email,
                                         password=self.password)
         self.read_update_url = reverse("user-detail", kwargs={"pk": user.id})
         self.put = json.dumps({
-            "template": {"data": [{"name": "password", "value": "updated_pass"}]}})
+            "template": {"data": [{"name": "password", "value": "updated_pass"},
+                                  {"name": "email", "value": "dev1@babymri.org"}]}})
 
     def test_user_detail_success(self):
         self.client.login(username=self.username, password=self.password)
         response = self.client.get(self.read_update_url)
         self.assertContains(response, self.username)
+        self.assertContains(response, self.email)
 
     def test_user_detail_failure_unauthenticated(self):
         response = self.client.get(self.read_update_url)
@@ -99,6 +88,7 @@ class UserDetailViewTests(UserViewTests):
         response = self.client.put(self.read_update_url, data=self.put,
                                    content_type=self.content_type)
         self.assertContains(response, self.username)
+        self.assertContains(response, "dev1@babymri.org")
 
     def test_user_update_failure_unauthenticated(self):
         response = self.client.put(self.read_update_url, data=self.put,
@@ -106,7 +96,8 @@ class UserDetailViewTests(UserViewTests):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_user_update_failure_access_denied(self):
-        User.objects.create_user(username="other_username", password="other_password")
+        User.objects.create_user(username="other_username", email="dev2@babymri.org",
+                                 password="other_password")
         self.client.login(username="other_username", password="other_password")
         response = self.client.put(self.read_update_url, data=self.put,
                                    content_type=self.content_type)
