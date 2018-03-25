c64e79deb0b06a8bd719187fb8bb00d4cbf7d7e3
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-03-25 09:51:30 -0400

    Set charm object debugging via settings.

--- chris_backend/plugins/services/manager.py
+++ chris_backend/plugins/services/manager.py
@@ -27,7 +27,6 @@ from plugins.models import ComputeResource
 from django.conf import settings
 
 
-
 class PluginManager(object):
     def __init__(self):
         parser = ArgumentParser(description='Manage plugins')
