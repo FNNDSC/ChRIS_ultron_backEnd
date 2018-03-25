bff6dedd3a3580eb2472f3226c590b62f264542b
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-03-25 09:51:30 -0400

    Set charm object debugging via settings.

--- chris_backend/plugins/services/manager.py
+++ chris_backend/plugins/services/manager.py
@@ -24,6 +24,7 @@ from plugins.models import TYPES, PLUGIN_TYPE_CHOICES, STATUS_TYPES
 from plugins.fields import CPUInt, MemoryInt
 from plugins.services import charm
 from plugins.models import ComputeResource
+from django.conf import settings
 
 
 class PluginManager(object):
@@ -362,7 +363,7 @@ class PluginManager(object):
             inputdir    = inputdirManagerFS,
             outputdir   = outputdirManagerFS,
             IOPhost     = self.str_IOPhost,
-            quiet       = True
+            quiet       = settings.CHRIS_DEBUG['quiet']
         )
 
         # Some dev notes...
