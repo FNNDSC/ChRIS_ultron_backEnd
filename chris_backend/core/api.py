08b895cbb6f85317272d0ece37688689a6521ff8
Jorge <jbernal0019@yahoo.es>
2019-01-11 16:55:01 -0500

    Change relationship between PluginInstance and Feed from one-to-one to many-to-one

--- chris_backend/core/api.py
+++ chris_backend/core/api.py
@@ -122,6 +122,13 @@ urlpatterns = format_suffix_patterns([
     url(r'^v1/plugins/instances/files/(?P<pk>[0-9]+)/.*$',
         plugin_views.FileResource.as_view(), name='plugininstancefile-resource'),
 
+    url(r'^v1/plugins/instances/files/(?P<pk>[0-9]+)/$',
+        plugin_views.PluginInstanceFileDetail.as_view(),
+        name='plugininstance-file-detail'),
+
+    url(r'^v1/plugins/instances/files/(?P<pk>[0-9]+)/.*$',
+        plugin_views.FileResource.as_view(), name='plugininstance-file-resource'),
+
     url(r'^v1/plugins/instances/(?P<pk>[0-9]+)/parameters/$',
         plugininstance_views.PluginInstanceParameterList.as_view(),
         name='plugininstance-parameter-list'),
