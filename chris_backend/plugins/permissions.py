82b259f72f51ab7c5faba8dd53f3b27db3c8cf7c
Jorge <jbernal0019@yahoo.es>
2019-01-14 17:08:57 -0500

    Remove unnecessary IsChrisOrReadOnly permission class and fix IsRelatedFeedOwnerOrChris premission class so it can work with both plugin instance and plugin instance file objects

--- chris_backend/plugins/permissions.py
+++ chris_backend/plugins/permissions.py
@@ -1,46 +1,21 @@
 from rest_framework import permissions
 
 
-class IsChrisOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission to only allow superuser 'chris' to modify/edit it.
-    Read only is allowed to other users.
-    """
-
-    def has_permission(self, request, view):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Write permissions are only allowed to the superuser 'chris'.
-        return request.user.username == 'chris' 
-
-    def has_object_permission(self, request, view, obj):
-        return self.has_permission(request, view)
-
-
 class IsRelatedFeedOwnerOrChris(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object or superuser
-    'chris' to modify/edit a feed-related object (eg. a note).
+    'chris' to access a feed-related object (eg. a plugin instance).
     """
 
     def has_object_permission(self, request, view, obj):
-        # Read and write permissions are only allowed to
-        # the owner and superuser 'chris'.
+        # Access is only allowed to related feed owners and superuser 'chris'.
         #import pdb; pdb.set_trace()
         if request.user.username == 'chris':
             return True
-        if hasattr(obj.plugin_inst.feed, 'all'):
-            owner_lists = [feed.owner.all() for feed in obj.plugin_inst.feed.all()]
-            for owner_list in owner_lists:
-                if request.user in owner_list:
-                    return True
-        elif request.user in obj.plugin_inst.feed.owner.all():
+        if hasattr(obj, 'plugin_inst'):
+            related_feed = obj.plugin_inst.feed
+        else:
+            related_feed = obj.feed
+        if request.user in related_feed.owner.all():
             return True
         return False
-
-
-
-
