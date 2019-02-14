0a34cf2ee09dea8c8faf4f782814bb054695519c
Jorge <jbernal0019@yahoo.es>
2019-02-14 16:54:26 -0500

    Add get_pipings_tree method to Pipeline model

--- chris_backend/plugins/models.py
+++ chris_backend/plugins/models.py
@@ -202,6 +202,28 @@ class Pipeline(models.Model):
                                 for param in plg.parameters.all()])
         return param_names
 
+    def get_pipings_tree(self):
+        """
+        Custom method to return a dictionary containing a dictionary representing a tree
+        of pipings and the id of the piping that is the root of the tree. The keys of the
+        dictionary tree are the pipings' ids and the values are dictionaries containing
+        the piping and the list of child pipings' ids.
+        """
+        pipings = list(self.plugin_pipings.all())
+        root_pip = [pip for pip in pipings if not hasattr(pip, 'previous')][0]
+        root_id = root_pip.id
+        tree = {}
+        tree[root_id] = {'piping': root_pip, 'child_ids':[]}
+        for pip in pipings:
+            if pip.id not in tree:
+                tree[pip.id] = {'piping': pip, 'child_ids': []}
+                prev_id = pip.previous.id
+                if prev_id in tree:
+                    tree[prev_id]['child_ids'].append(pip.id)
+                else:
+                    tree[prev_id] = {'piping': pip.previous, 'child_ids': [pip.id]}
+        return {'root_id': root_id, 'tree': tree}
+
     @staticmethod
     def get_accesible_pipelines(user):
         """
