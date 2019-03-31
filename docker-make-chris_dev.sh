06be73014b0d29aa05507738c3e43654fc59f508
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2019-03-31 18:30:21 -0400

    Add pull for plugins
    Pull ChRIS plugins.
    
    Fix formatting

--- docker-make-chris_dev.sh
+++ docker-make-chris_dev.sh
@@ -359,7 +359,7 @@ else
     # Add a new plugin name to the list for it to be automatically registered
     # If the main module name within a plugin is different than the plugin name,
     # separate the plugin and module strings with a '/' characater (see below):
-    declare -a plugins=("simplefsapp"
+    declare -a plugins=( "simplefsapp"
                          "simpledsapp"
                          "pacsquery"
                          "pacsretrieve"
@@ -368,7 +368,32 @@ else
                          "dircopy"
                          "pfdicom_tagextract/dcm_tagExtract"
                          "pfdicom_tagsub/dcm_tagSub"
+                         "pacscopy"
+                         "mpcs"
+                         "freesurfer_pp"
+                         "z2labelmap"
+                         "mri10yr06mo01da_normal"
     )
+
+    title -d 1 "Checking on container plugins..."
+        if [[ $CREPO == "fnndsc" ]] ; then
+            echo "Pulling latest version of all fnndsc plugin containers..."
+            for plugin in "${plugins[@]}" ; do
+                printf "${Cyan}%-25s${NC} <--- ${LightBlue}[ dockerhub ]${NC}: " "[ $plugin ]"                
+                dockerhubRepo=$(echo $plugin | awk -F\/ '{print $1}')
+                moduleName=$(echo $plugin | awk -F\/ '{print $2}')
+                if (( ! ${#moduleName} )) ; then
+                    moduleName=$dockerhubRepo
+                fi
+                PLUGIN_DOCK="fnndsc/pl-${dockerhubRepo}"
+                CMD="docker pull $PLUGIN_DOCK"
+                echo -e "${White}$CMD${NC}"
+                echo $sep
+                echo $CMD | sh
+                echo $sep
+            done
+        fi
+    windowBottom
 
     title -d 1 "Automatically uploading some plugins to the ChRIS store ..."
     declare -i i=1
@@ -378,8 +403,8 @@ else
         if (( ! ${#moduleName} )) ; then
             moduleName=$dockerhubRepo
         fi
-        printf "${STEP}.$i: "
-        printf "${Cyan}%-25s${NC} ---> ${LightBlue}[ ChRIS store ]${NC}... " "[ $dockerhubRepo ]"
+        printf "%8s" "${STEP}.$i: "
+        printf "${Cyan}%-28s${NC} ---> ${LightBlue}[ ChRIS store ]${NC}... " "[ $dockerhubRepo ]"
         PLUGIN_DOCK="fnndsc/pl-${dockerhubRepo}"
         PLUGIN_REP=$(docker run --rm "$PLUGIN_DOCK" "${moduleName}.py" --json 2> /dev/null;)
         docker-compose exec chrisstore python plugins/services/manager.py add "${dockerhubRepo}" cubeadmin https://github.com/FNNDSC "$PLUGIN_DOCK" --descriptorstring "$PLUGIN_REP"
@@ -392,8 +417,8 @@ else
     declare -i i=1
     for plugin in "${plugins[@]}"; do
         dockerhubRepo=$(echo $plugin | awk -F\/ '{print $1}')
-        printf "${STEP}.$i:"
-        printf "${LightBlue}[ ChRIS store ]${NC}::${Cyan}%-25s${NC} --> ${Yellow}[ CUBE ]${NC}::${Cyan}$COMPUTEENV${NC}..." "[ $dockerhubRepo ]"
+        printf "%8s" "${STEP}.$i: "
+        printf "${LightBlue}[ ChRIS store ]${NC}::${Cyan}%-28s${NC} --> ${Yellow}[ CUBE ]${NC}::${Cyan}$COMPUTEENV${NC}..." "[ $dockerhubRepo ]"
         docker-compose exec chris_dev python plugins/services/manager.py add "${dockerhubRepo}" $COMPUTEENV http://chrisstore:8010/api/v1/
         printf "\t${LightGreen}[ success ]${NC}\n"
         ((i++))
