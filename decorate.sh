c2652cdc6426d6e43a89bad0fa37b8ae5b64a315
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2020-06-09 17:14:58 -0400

    Update with nicer shadow effect

--- decorate.sh
+++ decorate.sh
@@ -17,54 +17,60 @@ COLPUT(){ echo -en "\033[${1}G";}                       # put text in the same l
   MARK(){ echo -en "\033[7m";}                          # the same as 'tput smso'
 UNMARK(){ echo -en "\033[27m";}                         # the same as 'tput rmso'
   DRAW(){ echo -en "\033%@";echo -en "\033(0";}         # switch to 'garbage' mode
- # DRAW(){ echo -en "\033%";echo -en "\033(0";}          # switch to 'garbage' mode
+#  DRAW(){ echo -en "\033%";echo -en "\033(0";}          # switch to 'garbage' mode
  WRITE(){ echo -en "\033(B";}                           # return to normal mode from 'garbage' on the screen
   BLUE(){ echo -en "\033c\033[0;1m\033[37;44m\033[J";}  # reset screen, set background to blue and font to white
 
 # Foreground
 RED='\033[0;31m'
 NC='\033[m' # No Color
-Black='\033[0;30m'     
+Black='\033[0;30m'
 DarkGray='\033[1;30m'
-Red='\033[0;31m'     
+Red='\033[0;31m'
 LightRed='\033[1;31m'
-Green='\033[0;32m'     
+Green='\033[0;32m'
 LightGreen='\033[1;32m'
-Brown='\033[0;33m'     
+Brown='\033[0;33m'
 Yellow='\033[1;33m'
-Blue='\033[0;34m'     
+Blue='\033[0;34m'
 LightBlue='\033[1;34m'
-Purple='\033[0;35m'     
+Purple='\033[0;35m'
 LightPurple='\033[1;35m'
-Cyan='\033[0;36m'     
+Cyan='\033[0;36m'
 LightCyan='\033[1;36m'
-LightGray='\033[0;37m'     
+LightGray='\033[0;37m'
 White='\033[1;37m'
 
 # Background
 NC='\033[m' # No Color
-BlackBG='\033[0;40m'     
+BlackBG='\033[0;40m'
 DarkGrayBG='\033[1;40m'
-RedBG='\033[0;41m'     
+RedBG='\033[0;41m'
 LightRedBG='\033[1;41m'
-GreenBG='\033[0;42m'     
+GreenBG='\033[0;42m'
 LightGreenBG='\033[1;42m'
-BrownBG='\033[0;43m'     
+BrownBG='\033[0;43m'
 YellowBG='\033[1;43m'
-BlueBG='\033[0;44m'     
+BlueBG='\033[0;44m'
 LightBlueBG='\033[1;44m'
-PurpleBG='\033[0;45m'     
+PurpleBG='\033[0;45m'
 LightPurpleBG='\033[1;45m'
-CyanBG='\033[0;46m'     
+CyanBG='\033[0;46m'
 LightCyanBG='\033[1;46m'
-LightGrayBG='\033[0;47m'     
+LightGrayBG='\033[0;47m'
 WhiteBG='\033[1;47m'
+
+function center {
+  termwidth=80
+  padding="$(printf '%0.1s' ={1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
 
 function title {
     declare -i b_date=0
     local OPTIND
     while getopts "d:" opt; do
-        case $opt in 
+        case $opt in
             d) b_date=$OPTARG ;;
         esac
     done
@@ -82,6 +88,9 @@ function title {
     if (( ${#MSG2} )) ; then
         TITLE2=$(echo " $MSG2 ")
         LEN2=$(echo "$TITLE2" | awk -F\| {'printf("%s", length($1));'})
+        if ! (( LEN2 % 2 )) ; then
+            TITLE2="$TITLE2 "
+        fi
         MSG2=$(echo -e "$TITLE2" | awk -F\| {'printf("%*s%*s\n", 40+length($1)/2, $1, 41-length($1)/2, "");'})
     fi
     printf "\n"
@@ -89,12 +98,14 @@ function title {
     lDATE=${#DATE}
     DRAW
     if (( b_date )) ; then
-        printf "${LightBlue}l" ;  for c in $(seq 1 $lDATE); do printf "q" ; done ; 
+        printf "${NC}${White}"
+        printf "${White}l" ;  for c in $(seq 1 $lDATE); do printf "q" ; done ;
         printf "k\nx"
         WRITE
+        printf "${NC}${LightBlue}"
         printf "%-30s" "$DATE"
-        DRAW 
-        printf "x\n"
+        DRAW
+        printf "${White}x${NC}\n"
         printf "${Yellow}t" ; for c in $(seq 1 $lDATE); do printf "q";  done ; printf "v"
         REM=$(expr 79 - $lDATE)
         for c in $(seq 1 $REM); do printf "q" ; done ; printf "k\n"
@@ -108,26 +119,21 @@ function title {
     printf "${LightPurple}$MSG${Yellow}"
     if (( ${#MSG2} )) ; then
         DRAW
-        printf "x\nx"
+        printf "x${Brown}a\n${Yellow}x"
         WRITE
         printf "${LightPurple}$MSG2${Yellow}"
     fi
     DRAW
-    printf "x\n"
-    printf "${Yellow}t" ;  for c in $(seq 1 80); do printf "q" ; done ; printf "u\n"
+    printf "x${Brown}a\n"
+    printf "${Yellow}t" ;  for c in $(seq 1 80); do printf "q" ; done ; printf "u${Brown}a\n"
     WRITE
     printf "${NC}"
 }
 
 function windowBottom {
-    # printf "a b c d f e f g h i j k l m n o p q r s t u v w x y z]\n"
     DRAW
-    # printf "a b c d f e f g h i j k l m n o p q r s t u v w x y z]\n"
-    printf "${Yellow}mw" ;  for c in $(seq 1 79); do printf "q" ; done ; printf "v${Brown}qk\n"
-    # printf "mwqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqv${Brown}qk\n"
-    # printf "mqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj\n"
-    printf "${Brown} m" ; for c in $(seq 1 81); do printf "q" ; done ; printf "j\n"
-    # printf " mqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj\n"
+    printf "${Yellow}mq" ;  for c in $(seq 1 79); do printf "q" ; done ; printf "j${Brown}a\n"
+    printf "${Brown} a" ; for c in $(seq 1 81); do printf "a" ; done ; printf "\n"
     WRITE
     printf "${NC}"
 }
