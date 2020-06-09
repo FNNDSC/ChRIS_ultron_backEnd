94565070108bf5d3660975bf325b9938c73fc4d6
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2020-06-09 17:20:04 -0400

    Beautify

--- docker-destroy.sh
+++ docker-destroy.sh
@@ -4,21 +4,30 @@ source ./decorate.sh
 
 declare -i STEP=0
 
-title -d 1 "Destroying containerized development environment" "from ./docker-compose_dev.yml]"
-echo
-printf "Do you want to also remove persistent volumes?"
-read -p  " [y/n] " -n 1 -r
-echo
-echo
-if [[ $REPLY =~ ^[Yy]$ ]] ; then
-    docker-compose -f docker-compose_dev.yml down -v
-    echo "Removing ./FS tree"
-    rm -fr ./FS
-else
-    docker-compose -f docker-compose_dev.yml down
-fi
+title -d 1 "Destroying containerized development environment" "from  ./docker-compose_dev.yml"
+    echo "Do you want to also remove persistent volumes? [y/n]"             | boxes.sh
+    windowBottom
+    old_stty_cfg=$(stty -g)
+    stty raw -echo ; REPLY=$(head -c 1) ; stty $old_stty_cfg
+    echo -en "\033[2A\033[2K"
+    # read -p  " " -n 1 -r REPLY
+    if [[ $REPLY =~ ^[Yy]$ ]] ; then
+        printf "Removing persisent volumes...\n" | boxes.sh ${LightRed}
+        echo "This might take a few minutes... please be patient."          | boxes.sh ${LightRed}
+        windowBottom
+        docker-compose --no-ansi -f docker-compose_dev.yml down -v |& tee dc.out >/dev/null
+        echo -en "\033[2A\033[2K"
+        cat dc.out | boxes.sh
+        echo "Removing ./FS tree"                                           | boxes.sh
+        rm -fr ./FS
+    else
+        printf "Keeping persistent volumes...\n"                            | boxes.sh ${LightGreen}
+        windowBottom
+        echo -en "\033[2A\033[2K"
+    fi
 windowBottom
 
 title -d 1 "Stopping swarm cluster..."
-    docker swarm leave --force
+    docker swarm leave --force >dc.out 2>dc.out
+    cat dc.out                                                              | boxes.sh
 windowBottom
