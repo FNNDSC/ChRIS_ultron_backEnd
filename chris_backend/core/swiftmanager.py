c678d108de7a314698c0817a162313c3aac8eadb
Jennings Zhang <jenni_zh@protonmail.com>
2023-06-02 04:32:42 -0400

    Minimize public interface of SwiftManager

--- chris_backend/core/swiftmanager.py
+++ chris_backend/core/swiftmanager.py
@@ -22,7 +22,7 @@ class SwiftManager(object):
         # swift storage connection object
         self._conn = None
 
-    def get_connection(self):
+    def __get_connection(self):
         """
         Connect to swift storage and return the connection object.
         """
@@ -43,22 +43,29 @@ class SwiftManager(object):
         """
         Create the storage container.
         """
-        conn = self.get_connection()
+        conn = self.__get_connection()
         try:
             conn.put_container(self.container_name)
         except ClientException as e:
             logger.error(str(e))
             raise
 
-    def ls(self, path, **kwargs):
+    def ls(self, path):
         """
         Return a list of objects in the swift storage with the provided path
         as a prefix.
         """
-        b_full_listing = kwargs.get('full_listing', True)
+        return self._ls(path, b_full_listing=True)
+
+    def _ls(self, path, b_full_listing: bool):
+        """
+        Note to developers: the body of ``_ls`` was originally the body of ``self.ls``,
+        though it's been renamed to ``_ls`` so that ``self.ls``'s signature could be
+        changed. ``self.ls`` originally accepted ``**kwargs`` but that is no longer the case.
+        """
         l_ls = []  # listing of names to return
         if path:
-            conn = self.get_connection()
+            conn = self.__get_connection()
             for i in range(5):
                 try:
                     # get the full list of objects in Swift storage with given prefix
@@ -79,13 +86,13 @@ class SwiftManager(object):
         """
         Return True/False if passed path exists in swift storage.
         """
-        return len(self.ls(path, full_listing=False)) > 0
+        return len(self._ls(path, b_full_listing=False)) > 0
 
     def obj_exists(self, obj_path):
         """
         Return True/False if passed object exists in swift storage.
         """
-        conn = self.get_connection()
+        conn = self.__get_connection()
         for i in range(5):
             try:
                 conn.head_object(self.container_name, obj_path)
@@ -100,17 +107,17 @@ class SwiftManager(object):
             else:
                 return True
 
-    def upload_obj(self, swift_path, contents, **kwargs):
+    def upload_obj(self, swift_path, contents, content_type=None):
         """
         Upload an object (a file contents) into swift storage.
         """
-        conn = self.get_connection()
+        conn = self.__get_connection()
         for i in range(5):
             try:
                 conn.put_object(self.container_name,
                                 swift_path,
                                 contents=contents,
-                                **kwargs)
+                                content_type=content_type)
             except ClientException as e:
                 logger.error(str(e))
                 if i == 4:
@@ -119,15 +126,14 @@ class SwiftManager(object):
             else:
                 break
 
-    def download_obj(self, obj_path, **kwargs):
+    def download_obj(self, obj_path):
         """
         Download an object from swift storage.
         """
-        conn = self.get_connection()
+        conn = self.__get_connection()
         for i in range(5):
             try:
-                resp_headers, obj_contents = conn.get_object(self.container_name,
-                                                             obj_path, **kwargs)
+                resp_headers, obj_contents = conn.get_object(self.container_name, obj_path)
             except ClientException as e:
                 logger.error(str(e))
                 if i == 4:
@@ -136,15 +142,15 @@ class SwiftManager(object):
             else:
                 return obj_contents
 
-    def copy_obj(self, obj_path, dest_path, **kwargs):
+    def copy_obj(self, obj_path, dest_path):
         """
         Copy an object to a new destination in swift storage.
         """
-        conn = self.get_connection()
+        conn = self.__get_connection()
         dest = os.path.join('/' + self.container_name, dest_path.lstrip('/'))
         for i in range(5):
             try:
-                conn.copy_object(self.container_name, obj_path, dest, **kwargs)
+                conn.copy_object(self.container_name, obj_path, dest)
             except ClientException as e:
                 logger.error(str(e))
                 if i == 4:
@@ -157,7 +163,7 @@ class SwiftManager(object):
         """
         Delete an object from swift storage.
         """
-        conn = self.get_connection()
+        conn = self.__get_connection()
         for i in range(5):
             try:
                 conn.delete_object(self.container_name, obj_path)
@@ -168,33 +174,3 @@ class SwiftManager(object):
                 time.sleep(0.4)
             else:
                 break
-
-    def upload_files(self, local_dir, swift_prefix='', **kwargs):
-        """
-        Upload all the files within a local directory recursively to swift storage.
-
-        By default, the location in swift storage will map 1:1 to the location of
-        files in the local filesytem. This location can be remapped by using the
-        <swift_prefix>. For example, assume a local directory /home/user/project/data/
-        with the following files:
-
-            '/home/user/project/data/file1',
-            '/home/user/project/data/dir1/file_d1',
-            '/home/user/project/data/dir2/file_d2'
-
-        and we want to upload everything in that directory to object storage, at location
-        '/storage'. In this case, swift_prefix='/storage' results in a new list
-
-            '/storage/file1',
-            '/storage/dir1/file_d1',
-            '/storage/dir2/file_d2'
-        """
-        # upload all files down the <local_dir>
-        for root, dirs, files in os.walk(local_dir):
-            swift_base = root.replace(local_dir, swift_prefix, 1) if swift_prefix else root
-            for filename in files:
-                swift_path = os.path.join(swift_base, filename)
-                if not self.obj_exists(swift_path):
-                    local_file_path = os.path.join(root, filename)
-                    with open(local_file_path, 'rb') as f:
-                        self.upload_obj(swift_path, f.read(), **kwargs)
