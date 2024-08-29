beb6f5fec3afaf47c31f999db152b375d6addf48
Jennings Zhang <dev@sl.jennin.xyz>
2024-08-29 11:50:36 -0400

    Remove obsolete scripts

--- docker-compose-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# change to directory where this script lives
-cd "$(dirname "$(readlink -f "$0")")"
-
-set -ex
-docker compose -f docker-compose-dev.yml up -d
-exec docker compose -f docker-compose-dev.yml run --rm $notty chrisomatic
