c05c884b68098c535486721e1fa5099d56964c21
Jennings Zhang <dev@sl.jennin.xyz>
2024-09-11 11:57:29 -0400

    Fix asgi.py
    https://github.com/django/channels/issues/1564#issuecomment-722354397

--- chris_backend/config/asgi.py
+++ chris_backend/config/asgi.py
@@ -10,17 +10,20 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
+# get_asgi_application MUST be called before other imports.
+# https://github.com/django/channels/issues/1564#issuecomment-722354397
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from core.websockets.urls import websocket_urlpatterns
 from core.websockets.auth import TokenQsAuthMiddleware
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
-django_asgi_app = get_asgi_application()
 
 # see https://channels.readthedocs.io/en/3.x/installation.html
 application = ProtocolTypeRouter({
