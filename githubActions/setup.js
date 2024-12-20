d49e4b5559242ac790e9b5a13944cfd12dbc5c80
Jennings Zhang <dev@sl.jennin.xyz>
2024-12-20 16:10:58 -0500

    Change just installation to use pipx

--- githubActions/setup.js
+++ githubActions/setup.js
@@ -1,7 +1,7 @@
 const child_process = require('child_process');
 
 child_process.execFileSync(
-  'npm',
-  ['install', '-g', 'rust-just'],
+  'pipx',
+  ['install', 'rust-just'],
   { stdio: 'inherit' }
 );
