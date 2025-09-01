1f9e7ed675281c71df263980c26839d4ad6923a7
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2025-09-01 17:47:54 -0400

    add django-debug-toolbar back

--- chris_backend/config/urls.py
+++ chris_backend/config/urls.py
@@ -47,3 +47,10 @@ urlpatterns = [
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
